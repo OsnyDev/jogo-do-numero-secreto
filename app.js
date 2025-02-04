98fc7f21f84751549ade6f08c26f976efb7b7f9b
OsnyDev <osnydantas70@gmail.com>
2025-02-03 21:30:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2}); //velocidade da voz (rate 1)
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2}); //velocidade da voz (rate 0.5)
 }
 
 //Outra Opção de Fala Web Speech API
